141468be1dec43aabed5f97510dfd3410bba0fcb
chenlinzhang3@gmail.com <chenlinzhang3@gmail.com>
2024-11-16 22:12:34 +0800

    feat(upload): 实现文件上传功能
    - 添加上传按钮和进度条组件
    - 实现 PDF 文件的上传和渲染
    - 集成 UploadThing 服务
    - 添加错误处理和提示

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
-
+import { Toaster } from "@/components/ui/toaster";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,6 +29,7 @@ export default function RootLayout({
             inter.className
           )}
         >
+          <Toaster />
           <Navbar />
           {children}
         </body>
--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,52 @@
-const PdfRenderer = () => {
+"use client";
+
+import { useToast } from "@/hooks/use-toast";
+import { ChevronDown, Loader2 } from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+interface PdfRendererProps {
+  url: string;
+}
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5"> TOP BAR</div>
+        <div className="flex items-center gap-1.5">
+          <Button variant="ghost" aria-label="previous page">
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+
+          <div className="flex items-center gap-1.5"></div>
+        </div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            file={url}
+            className=" max-h-full"
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try again later.",
+                variant: "destructive",
+              });
+            }}
+          >
+            <Page width={width ? width : 1} pageNumber={1} />
+          </Document>
+        </div>
       </div>
     </div>
   );
